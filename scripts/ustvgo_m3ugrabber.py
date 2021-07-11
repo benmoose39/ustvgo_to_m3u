0271788d563d0ad06830d94763baa215ff3537d6
benmoose39 <unnik1998@gmail.com>
2021-07-11 14:09:44 +0530

    Add files via upload

--- scripts/ustvgo_m3ugrabber.py
+++ scripts/ustvgo_m3ugrabber.py
@@ -14,13 +14,14 @@ import requests
 
 def grab(name, code, logo):
     data = {'stream': code}
-    m3u = requests.post('https://ustvgo.tv/data.php', data=data).text
+    m3u = s.post('https://ustvgo.tv/data.php', data=data).text
     print(f'\n#EXTINF:-1 tvg-id="{code}" group-title="ustvgo" tvg-logo="{logo}", {name}')
     print(m3u)
 
 
 print('#EXTM3U x-tvg-url="https://raw.githubusercontent.com/nomoney4me/ustvgo/main/output/ustvgo_epg.xml"')
 print(banner)
+s = requests.Session()
 with open('../ustvgo_channel_info.txt') as file:
     for line in file:
         line = line.strip()
